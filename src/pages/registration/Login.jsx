71e13892886f764466af192767713835af39c3ee
abhisek8094 <iasabhi41@gmail.com>
2024-04-23 07:05:12 +0530

    login commit

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -8,7 +8,7 @@ import Loader from "../../components/loader/Loader";
 
 function Login() {
   const context = useContext(myContext);
-  const { loading, setLoading , signUpSuccess} = context;
+  const { loading, setLoading, signUpSuccess } = context;
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -17,14 +17,15 @@ function Login() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(signUpSuccess && !autoFill){
+    if (signUpSuccess && !autoFill) {
       setEmail(signUpSuccess.email);
       setPassword(signUpSuccess.password);
       setAutoFill(true);
     }
   }, [signUpSuccess, autoFill]);
 
-  const login = async () => {
+  const login = async (e) => {
+    e.preventDefault();
     setLoading(true);
     try {
       const result = await signInWithEmailAndPassword(auth, email, password);
@@ -48,49 +49,49 @@ function Login() {
   };
 
   return (
-    <div className=" flex justify-center bg-slate-500 items-center h-screen">
+    <div className="flex justify-center bg-slate-500 items-center h-screen">
       {loading && <Loader />}
-      <div className=" bg-gray-800 px-10 py-10 rounded-xl ">
-        <form >
-        <div >
-          <h1 className="text-center text-white text-xl mb-4 font-bold">
-            Login
-          </h1>
-        </div>
-        <div>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            name="email"
-            className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
-            placeholder="Email"
-            autoComplete="email"
-          />
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
-            placeholder="Password"
-            autoComplete="password"
-          />
-        </div>
-        <div className=" flex justify-center mb-3">
-          <button
-            onClick={login}
-            className=" bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg"
-          >
-            Login
-          </button>
-        </div>
+      <div className="bg-gray-800 px-10 py-10 rounded-xl">
+        <form onSubmit={login}>
+          <div>
+            <h1 className="text-center text-white text-xl mb-4 font-bold">
+              Login
+            </h1>
+          </div>
+          <div>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              className="bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Email"
+              autoComplete="email"
+            />
+          </div>
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
+              placeholder="Password"
+              autoComplete="password"
+            />
+          </div>
+          <div className="flex justify-center mb-3">
+            <button
+              type="submit"
+              className="bg-yellow-500 w-full text-black font-bold  px-2 py-2 rounded-lg"
+            >
+              Login
+            </button>
+          </div>
         </form>
         <div>
           <h2 className="text-white">
-            Don't have an account ? {" "}
-            <Link className=" text-yellow-500 font-bold" to={"/signup"}>
+            Don't have an account ?{" "}
+            <Link className="text-yellow-500 font-bold" to={"/signup"}>
               Signup
             </Link>
           </h2>
