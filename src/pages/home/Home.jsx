cd0c2bbf21908f46b4b04b1d4edd455834fe0103
abhisek8094 <iasabhi41@gmail.com>
2024-05-16 10:17:39 +0530

    Home updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,6 +6,7 @@ import img2 from "../../assets/img2.jpg";
 import img3 from "../../assets/img3.jpg";
 import img4 from "../../assets/img4.jpg";
 import Products from "../../components/products/Products";
+import { Link } from "react-router-dom";
 import Testimonial from "../../components/testimonial/Testimonial";
 import Track from "../../components/track/Track";
 
@@ -15,6 +16,13 @@ const Home = () => {
     <Layout>
       <Crousel movingimg={images} />
       <Products />
+      <div className="flex justify-center -mt-10 mb-4">
+        <Link to="/shop">
+          <button className=" bg-gray-300 px-5 py-2 mt-20 rounded-xl">
+            See more
+          </button>
+        </Link>
+      </div>
       <Track />
       <Testimonial />
     </Layout>
