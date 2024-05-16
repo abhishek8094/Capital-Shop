8d600cf48a7d6f8c2d325870ae5e62d2d20e3e97
abhisek8094 <iasabhi41@gmail.com>
2024-05-16 10:17:21 +0530

    Testimonial commit

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -2,29 +2,28 @@ import React, { useContext } from "react";
 import myContext from "../../context/myContext";
 
 function Testimonial() {
-    
   const context = useContext(myContext);
   const { mode } = context;
 
   return (
     <div>
       <section>
-        <div className=" container mx-auto px-5 py-10">
+        <div className="container mx-auto px-5 py-10">
           <h1
-            className=" text-center text-3xl font-bold text-black"
-            style={{ color: mode === "dark" ? "white" : "" }}
+            className="text-center text-3xl font-bold"
+            style={{ color: mode === "dark" ? "white" : "black" }}
           >
             Testimonial
           </h1>
           <h2
-            className=" text-center text-2xl font-semibold mb-10"
-            style={{ color: mode === "dark" ? "white" : "" }}
+            className="text-center text-2xl font-semibold mb-10"
+            style={{ color: mode === "dark" ? "white" : "black" }}
           >
-            What our <span className=" text-pink-500">customers</span> are
+            What our <span className="text-pink-500">customers</span> are
             saying
           </h2>
           <div className="flex flex-wrap -m-4">
-            <div className="sm:w-1/3 lg:mb-0 mb-6 p-4">
+            <div className="p-4 sm:w-1/2 lg:w-1/3 w-full">
               <div className="h-full text-center">
                 <img
                   alt="testimonial"
@@ -32,28 +31,28 @@ function Testimonial() {
                   src="https://t4.ftcdn.net/jpg/02/45/56/35/240_F_245563558_XH9Pe5LJI2kr7VQuzQKAjAbz9PAyejG1.jpg"
                 />
                 <p
-                  style={{ color: mode === "dark" ? "white" : "" }}
                   className="leading-relaxed"
+                  style={{ color: mode === "dark" ? "white" : "black" }}
                 >
                   Lorem ipsum dolor sit, amet consectetur adipisicing.
                 </p>
                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                 <h2
-                  style={{ color: mode === "dark" ? "#ff4162" : "" }}
                   className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase"
+                  style={{ color: mode === "dark" ? "#ff4162" : "black" }}
                 >
                   Shivam sir
                 </h2>
                 <p
-                  style={{ color: mode === "dark" ? "white" : "" }}
                   className="text-gray-500"
+                  style={{ color: mode === "dark" ? "white" : "gray" }}
                 >
                   Frontend Developer
                 </p>
               </div>
             </div>
 
-            <div className="sm:w-1/3 lg:mb-0 mb-6 p-4">
+            <div className="p-4 sm:w-1/2 lg:w-1/3 w-full">
               <div className="h-full text-center">
                 <img
                   alt="testimonial"
@@ -61,28 +60,28 @@ function Testimonial() {
                   src="https://t3.ftcdn.net/jpg/03/28/19/46/240_F_328194664_RKSHvMLgHphnD1nwQYb4QKcNeEApJmqa.jpg"
                 />
                 <p
-                  style={{ color: mode === "dark" ? "white" : "" }}
                   className="leading-relaxed"
+                  style={{ color: mode === "dark" ? "white" : "black" }}
                 >
                   Lorem ipsum dolor sit, amet consectetur adipisicing.
                 </p>
                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                 <h2
-                  style={{ color: mode === "dark" ? "#ff4162" : "" }}
                   className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase"
+                  style={{ color: mode === "dark" ? "#ff4162" : "black" }}
                 >
                   Nishant sir
                 </h2>
                 <p
-                  style={{ color: mode === "dark" ? "white" : "" }}
                   className="text-gray-500"
+                  style={{ color: mode === "dark" ? "white" : "gray" }}
                 >
                   React Js
                 </p>
               </div>
             </div>
 
-            <div className="sm:w-1/3 lg:mb-0 p-4">
+            <div className="p-4 lg:w-1/3 w-full">
               <div className="h-full text-center">
                 <img
                   alt="testimonial"
@@ -90,23 +89,23 @@ function Testimonial() {
                   src="https://t3.ftcdn.net/jpg/01/29/53/60/240_F_129536040_xSh3RrAMtGgME7K8fcNnEo8dKOnd3zsr.jpg"
                 />
                 <p
-                  style={{ color: mode === "dark" ? "white" : "" }}
                   className="leading-relaxed"
+                  style={{ color: mode === "dark" ? "white" : "black" }}
                 >
                   Lorem ipsum dolor sit amet consectetur adipisicing.
                 </p>
                 <span className="inline-block h-1 w-10 rounded bg-pink-500 mt-6 mb-4" />
                 <h2
-                  style={{ color: mode === "dark" ? "#ff4162" : "" }}
                   className="text-gray-900 font-medium title-font tracking-wider text-sm uppercase"
+                  style={{ color: mode === "dark" ? "#ff4162" : "black" }}
                 >
                   Abhishek Sir
                 </h2>
                 <p
-                  style={{ color: mode === "dark" ? "white" : "" }}
                   className="text-gray-500"
+                  style={{ color: mode === "dark" ? "white" : "gray" }}
                 >
-                  Frontend devloper
+                  Frontend Developer
                 </p>
               </div>
             </div>
