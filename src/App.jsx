64bdfe4c7a13cd3a542f394b33a7df6fead9f9bf
abhisek8094 <iasabhi41@gmail.com>
2024-05-16 10:15:43 +0530

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./pages/home/Home";
+import Shop from "./pages/shop/Shop";
 import { Routes, Route } from "react-router-dom";
 import Order from "./pages/order/Order";
 import MyState from "./context/myState";
@@ -19,6 +20,7 @@ const App = () => {
       <WishlistProvider>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element = {<Shop/>}/>
           <Route path="/wishlist" element={<Wishlist />} />
           <Route path="/order" element={<Order />} />
           <Route path="/cart" element={<Cart />} />
