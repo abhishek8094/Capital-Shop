6cd4dfdef20e0f03800cd358e11663fce309c775
abhisek8094 <iasabhi41@gmail.com>
2024-05-17 15:57:26 +0530

    Shop commit

--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext, useEffect } from "react";
 import Layout from "../../components/layout/Layout";
 import Filter from "../../components/filter/Filter";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
@@ -6,12 +6,21 @@ import { WishlistContext } from "../../context/WishlistContext";
 import myContext from "../../context/myContext";
 
 const Shop = () => {
-  const [products, setProducts] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const [sortOrder, setSortOrder] = useState("");
+  const {
+    mode,
+    products,
+    setProducts,
+    sortOrder,
+    setSortOrder,
+    selectedCategory,
+    setSelectedCategory,
+    searchQuery,
+    categories,
+    setCategories,
+  } = useContext(myContext);
+
   const { addToWishlist, removeFromWishlist, isInWishlist } =
     useContext(WishlistContext);
-  const { mode, categories, setCategories, } = useContext(myContext);
 
   async function getProducts(category = "") {
     const response = await fetch(
@@ -90,43 +99,46 @@ const Shop = () => {
             <div className="h-1 w-24 bg-pink-600 mx-auto rounded"></div>
           </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {products.slice(0, 8).map((product) => (
-              <div
-                key={product.id}
-                className="bg-slate-200 rounded-lg overflow-hidden shadow-md p-4 transition-transform duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-              >
-                <div className="relative">
-                  <div className="absolute top-2 right-2">
-                    {isInWishlist(product.id) ? (
-                      <FaHeart
-                        onClick={() => toggleWishlist(product)}
-                        className="text-red-500 text-xl cursor-pointer"
-                      />
-                    ) : (
-                      <FaRegHeart
-                        onClick={() => toggleWishlist(product)}
-                        className="text-xl cursor-pointer"
-                      />
-                    )}
+            {products
+              .filter((obj) => obj.title.toLowerCase().includes(searchQuery))
+              .slice(0, 8)
+              .map((product) => (
+                <div
+                  key={product.id}
+                  className="bg-slate-200 rounded-lg overflow-hidden shadow-md p-4 transition-transform duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+                >
+                  <div className="relative">
+                    <div className="absolute top-2 right-2">
+                      {isInWishlist(product.id) ? (
+                        <FaHeart
+                          onClick={() => toggleWishlist(product)}
+                          className="text-red-500 text-xl cursor-pointer"
+                        />
+                      ) : (
+                        <FaRegHeart
+                          onClick={() => toggleWishlist(product)}
+                          className="text-xl cursor-pointer"
+                        />
+                      )}
+                    </div>
+                    <img
+                      src={product.thumbnail}
+                      alt={product.title}
+                      className="rounded-lg w-full h-48 object-cover cursor-pointer"
+                      onClick={() => goToProductInfoPage(product.id)}
+                    />
                   </div>
-                  <img
-                    src={product.thumbnail}
-                    alt={product.title}
-                    className="rounded-lg w-full h-48 object-cover cursor-pointer"
-                    onClick={() => goToProductInfoPage(product.id)}
-                  />
+                  <div className="mt-4">
+                    <h1 className="text-lg uppercase font-bold">
+                      {product.title}
+                    </h1>
+                    <p className="mt-2 text-gray-600 text-sm">
+                      {product.description.slice(0, 60)}...
+                    </p>
+                    <p className="mt-2 text-gray-600">₹ {product.price}</p>
+                  </div>
                 </div>
-                <div className="mt-4">
-                  <h1 className="text-lg uppercase font-bold">
-                    {product.title}
-                  </h1>
-                  <p className="mt-2 text-gray-600 text-sm">
-                    {product.description.slice(0, 60)}...
-                  </p>
-                  <p className="mt-2 text-gray-600">₹ {product.price}</p>
-                </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </div>
