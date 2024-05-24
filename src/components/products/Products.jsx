9bc880c14471ffbb1bd42069c769d8eced596f82
abhisek8094 <iasabhi41@gmail.com>
2024-05-24 19:44:17 +0530

    Products commit

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -13,6 +13,7 @@ export default function Products() {
   async function getProducts() {
     const response = await fetch("https://dummyjson.com/products");
     const data = await response.json();
+    console.log(data);
     setProducts(data.products);
   }
 
@@ -44,44 +45,48 @@ export default function Products() {
         <div className="h-1 w-52 bg-pink-600 mx-auto rounded"></div>
       </div>
       <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {products
+          .filter((obj) => obj.category.toLowerCase().includes(searchQuery))
+          .filter((obj) => obj.title.toUpperCase().includes(searchQuery) )
 
-        {products.filter((obj) => obj.title.toLowerCase().includes(searchQuery))
-        .slice(0, 16).map((product) => (
-          <div
-            key={product.id}
-            className="bg-slate-200 rounded-lg m-3 overflow-hidden shadow-md px-8 py-8 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-            style={{ boxShadow: "0 4px 6px rgba(255,255,255,0.1)" }}
-          >
-            <div className="absolute top-0 right-0 mt-2 mr-2 md:mr-4 lg:mr-6">
-              {isInWishlist(product.id) ? (
-                <FaHeart
-                  onClick={() => toggleWishlist(product)}
-                  className="text-purple-700 text-xl cursor-pointer "
+          .map((product) => (
+            <div
+              key={product.id}
+              className="bg-slate-200 rounded-lg m-3 overflow-hidden shadow-md px-8 py-8 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
+              style={{ boxShadow: "0 4px 6px rgba(255,255,255,0.1)" }}
+            >
+              <div className="absolute top-0 right-0 mt-2 mr-2 md:mr-4 lg:mr-6">
+                {isInWishlist(product.id) ? (
+                  <FaHeart
+                    onClick={() => toggleWishlist(product)}
+                    className="text-purple-700 text-xl cursor-pointer "
+                  />
+                ) : (
+                  <FaRegHeart
+                    onClick={() => toggleWishlist(product)}
+                    className="text-xl font-medium text-red-800  cursor-pointer"
+                  />
+                )}
+              </div>
+              <div onClick={() => goToProductInfoPage(product.id)}>
+                <img
+                  src={product.thumbnail}
+                  alt={product.title}
+                  className="rounded-lg w-full h-48 cursor-pointer"
                 />
-              ) : (
-                <FaRegHeart
-                  onClick={() => toggleWishlist(product)}
-                  className="text-xl font-medium text-red-800  cursor-pointer"
-                />
-              )}
-            </div>
-            <div onClick={() => goToProductInfoPage(product.id)}>
-              <img
-                src={product.thumbnail}
-                alt={product.title}
-                className="rounded-lg w-full h-48 cursor-pointer"
-              />
 
-              <div className="mt-4">
-                <h1 className="text-lg uppercase font-bold">{product.title}</h1>
-                <p className="mt-2 text-gray-600 text-sm">
-                  {product.description.slice(0, 45)}...
-                </p>
-                <p className="mt-2 text-gray-600">₹ {product.price}</p>
+                <div className="mt-4">
+                  <h1 className="text-lg uppercase font-bold">
+                    {product.title}
+                  </h1>
+                  <p className="mt-2 text-gray-600 text-sm">
+                    {product.description.slice(0, 45)}...
+                  </p>
+                  <p className="mt-2 text-gray-600">₹ {product.price}</p>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
